fd4f537f71829a49cda0b739303c299232cb260c
Tammy Lam <lamtammy1515@gmail.com>
2020-01-31 17:14:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,5 +18,7 @@ def remove_strawberry(contacts)
 contacts.each do |person, data|
      data.each do |attribute, value|
        
+    if attribute == :favorite_ice_cream_flavors
+      value.each do |flavor|
        
 end
